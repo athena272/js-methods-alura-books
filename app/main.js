d410b23c7e19b6726fb9a26676375efbc6ca7eae
rosario <58920070+athena272@users.noreply.github.com>
2026-01-01 20:18:07 -0300

    chore: passar totalContainer para inicialização e eventos

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 import { bindBooksEvents, initBooks } from "./controllers/booksController.js";
 
 const booksContainer = document.querySelector('#livros');
+const totalContainer = document.querySelector('#valor');
 
-await initBooks({ container: booksContainer });
-bindBooksEvents({ container: booksContainer });
+await initBooks({ container: booksContainer, totalContainer });
+bindBooksEvents({ container: booksContainer, totalContainer });
